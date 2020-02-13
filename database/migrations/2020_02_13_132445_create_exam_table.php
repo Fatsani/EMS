9ce9650679c9da672643c1f510c52bb9bdebf03e
fatsani <bysonlawrence@gmail.com>
2020-02-13 15:28:13 +0200

    migration for exams table

---
+++ database/migrations/2020_02_13_132445_create_exam_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam', function (Blueprint $table) {
+            $table->increment('idexam')->primary();
+            $table->string('name',45);
+            $table->string('course',10);
+            $table->date('date');
+            $table->timestamps();
+            $table->foreign('course')->references('courseCode')->on('courses')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exam');
+    }
+}
