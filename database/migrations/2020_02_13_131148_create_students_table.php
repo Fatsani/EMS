fe70219e65f4f13fe6f84b851a6cfc4e7d36f97a
fatsani <bysonlawrence@gmail.com>
2020-02-13 15:13:08 +0200

    migration for students table

---
+++ database/migrations/2020_02_13_131148_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->string('idstudent')->primary();
+            $table->string('fname',45);
+            $table->string('lname',45);
+            $table->date('dob');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
